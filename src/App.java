ca4828201cda685b1a4b9db31daa9151801276e7
Hafizh Athallah <hafizhhall@users.noreply.github.com>
2021-03-01 21:18:44 +0700

    CURD

---
+++ src/App.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws IOException {
+        Scanner terminalInput = new Scanner(System.in);
+        String pilihanUser;
+        boolean isLanjutkan =  true;
+
+        while(isLanjutkan){
+        
+        System.out.println("Database Perpustakaan\n");
+        System.out.println("1.\t Lihat seluruh buku");
+        System.out.println("2.\tCari data buku");
+        System.out.println("3.\tTambah data buku");
+        System.out.println("4.\tUbah data buku");
+        System.out.println("5.\tHapus data buku");
+            
+        System.out.print("\n\nPilihan anda: ");
+            pilihanUser = terminalInput.next();
+            switch (pilihanUser) {
+                case "1":
+                    System.out.println("\n=================");
+                    System.out.println("LIST SELURUH BUKU");
+                    System.out.println("=================");
+                    tampilkanData();
+                    break;
+                case "2":
+                    System.out.println("\n=========");
+                    System.out.println("CARI BUKU");
+                    System.out.println("=========");
+                    // cari data
+                    break;
+                case "3":
+                    System.out.println("\n================");
+                    System.out.println("TAMBAH DATA BUKU");
+                    System.out.println("================");
+                    // tambah data
+                    break;
+                case "4":
+                    System.out.println("\n==============");
+                    System.out.println("UBAH DATA BUKU");
+                    System.out.println("==============");
+                    // ubah data
+                    break;
+                case "5":
+                    System.out.println("\n===============");
+                    System.out.println("HAPUS DATA BUKU");
+                    System.out.println("===============");
+                    // hapus data
+                    break;
+                default:
+                    System.err.println("\nInput anda tidak ditemukan\nSilahkan pilih [1-5]");
+            }
+    
+        }
+    }
+    private static void tampilkanData() {
+        
+    }
+
+}
